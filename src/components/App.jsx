8113fe5cb9fa612fd3b5718cfabff0e162a44651
OlexandrBolotov <boltik4a9@gmail.com>
2025-03-18 12:12:53 +0200

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Profile from './Profile/Profile';
+import userData from './userData.json';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
+};
+
+export default App;
